0428a8c7435aadb9720d1feba7c569ffcb4f4b73
jxf <wofeiwofeifeifei@gmail.com>
2012-09-04 14:06:42 +0800

    delete component border

--- application/views/smarty/cmp_text2.php
+++ application/views/smarty/cmp_text2.php
@@ -18,7 +18,7 @@ $(function(){
 <{/if}>
 });
 </script>
-<div id="<{$id}>" class="chidopi_component3D" style="border:solid 1px #eee;display:<{$display}>; position:absolute; top: <{$top}>px; left: <{$left}>px; z-index:<{$zIndex}>;">
+<div id="<{$id}>" class="chidopi_component3D" style="display:<{$display}>; position:absolute; top: <{$top}>px; left: <{$left}>px; z-index:<{$zIndex}>;">
     <{if $hide == 'self'}>
     <a class="closebox" href="#" onclick="$(this).parent().hide();"></a>
     <{/if}>
